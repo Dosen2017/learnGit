1a53fb817ad4e521b1c6e676f8429792b83ff951
Shark <2066815257@qq.com>
2019-09-23 10:32:54 +0800

    test

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>hello, this is my first page test2!</h1>")
+	io.WriteString(w, "<h1>hello, this is my first page test3!</h1>")
 }
 
 func main() {
