8f07bee39950ab7e41419a01354b59b41b0c3db5
Shark <2066815257@qq.com>
2019-09-23 11:54:05 +0800

    sucess tow

--- developserver/deploy.sh
+++ developserver/deploy.sh
@@ -2,8 +2,8 @@
 
 kill -9 $(pgrep webserver)
 cd ../
-#git pull https://github.com/Dosen2017/learnGit.git
-git checkout webserver/webserver --ours  #更新二进制文件
+git pull https://github.com/Dosen2017/learnGit.git
+#git checkout webserver/webserver --ours  #更新二进制文件
 cd webserver
 chmod 777 webserver
 ./webserver &
