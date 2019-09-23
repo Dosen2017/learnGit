61388c389c44c01500bdf446a03373bb1028cbc3
Shark <2066815257@qq.com>
2019-09-23 11:25:22 +0800

    222

--- developserver/deploy.sh
+++ developserver/deploy.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
 
 kill -9 $(pgrep webserver)
-# cd ../
-# rm -f ./webserver/webserver
+cd ../
 git pull https://github.com/Dosen2017/learnGit.git
+git checkout webserver/webserver --ours  #更新二进制文件
 cd webserver
-
-# git checkout webserver --ours
-
 chmod 777 webserver
 ./webserver &
