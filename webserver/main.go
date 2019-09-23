bf29bab48fbf91680f8a6517ca836193ced0a4fe
Shark <2066815257@qq.com>
2019-09-23 12:09:39 +0800

    sucess tow

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>hello, this is my first page test success tow3!</h1>")
+	io.WriteString(w, "<h1>hello, this is my first page test success tow4!</h1>")
 }
 
 func main() {
