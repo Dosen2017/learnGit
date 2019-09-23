9d84c9ea1c1f88da793164bd1cb3054f3766b6eb
Shark <2066815257@qq.com>
2019-09-23 11:11:41 +0800

    sh

--- developserver/deploy.sh
+++ developserver/deploy.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
 kill -9 $(pgrep webserver)
-cd ../webserver
-rm -f webserver
 cd ../
+rm -f ./webserver/webserver
 git pull https://github.com/Dosen2017/learnGit.git
 cd webserver
 chmod 777 webserver
